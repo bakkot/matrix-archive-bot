ca6330e33453f5487eb45032e6a6299d35ace3f4
Kevin Gibbons <bakkot@gmail.com>
2021-05-31 21:42:18 -0700

    make resources external

--- render-html.js
+++ render-html.js
@@ -55,6 +55,11 @@ if (rooms.length > 0) {
   fs.mkdirSync(indexDir, { recursive: true });
   let index = renderDay(rooms, 'index', '', [], null, null);
   fs.writeFileSync(path.join(indexDir, 'index.html'), index, 'utf8');
+
+  let resourcesDir = path.join(__dirname, 'resources');
+  for (let file of fs.readdirSync(resourcesDir)) {
+    fs.copyFileSync(path.join(resourcesDir, file), path.join(indexDir, file));
+  }
 }
 
 function sanitizeRoomName(room) {
@@ -87,205 +92,15 @@ function postprocessHTML(html) {
 }
 
 function renderDay(rooms, room, day, events, prev, next) {
+  let isIndex = room === 'index';
+  let cssSrc = isIndex ? 'style.css' : '../style.css';
+  let jsSrc = isIndex ? 'logs.js' : '../logs.js';
+
   return `<!doctype html>
 <head>
   <title>${room === 'index' ? 'Matrix Logs' : `${room} on ${day}`}</title>
-  <style>
-  body {
-    background-color: #fafafa;
-  }
-  .wrapper {
-    display: flex;
-    position: absolute;
-    top: 5px;
-    bottom: 10px;
-    width: 100%;
-  }
-  .sidebar {
-    flex: 0 0 165px;
-    border-right: 1px solid #444;
-    padding-right: 10px;
-  }
-  .title {
-    text-align: center;
-  }
-  .room-list {
-    border-top: 1px solid #444;
-    padding-top: 2em;
-    padding-left: .1em;
-    margin-top: 1em;
-    list-style: none;
-    margin: 0px;
-  }
-  .room-list > li {
-    margin-top: .2em;
-  }
-  .room-list a {
-    color: #777;
-    text-decoration: none;
-  }
-  .room-list a:hover {
-    text-decoration: underline;
-  }
-  .current-room {
-    padding-left: .5em;
-    color: black !important;
-    text-decoration: none;
-  }
-  .footer {
-    position: absolute;
-    bottom: 0px;
-  }
-  .nav > span {
-    /* text-decoration gets dropped from floating elements */
-    text-decoration: underline;
-  }
-  .log {
-    overflow-y: auto;
-    padding-left: 1em;
-  }
-
-  table {
-    border-spacing: 0px;
-  }
-  td {
-    padding: .12em;
-  }
-  a {
-    color: green;
-  }
-  p {
-    margin: 0px;
-  }
-  .ts {
-    color: #777;
-    text-decoration: none;
-  }
-  .ts:hover {
-    text-decoration: underline;
-  }
-  .ts-cell {
-    vertical-align:top;
-  }
-  .nick-cell {
-    max-width: 10em;
-    text-align: right;
-    vertical-align:top;
-  }
-  .msg-cell {
-    padding-left: .3em;
-  }
-  .highlight {
-    background-color: #fffbdd;
-  }
-
-
-  blockquote {
-    margin: 0px;
-    border-left: 4px solid #ddd;
-    padding-left: .3em;
-  }
-  .nick-1 { color: #f25e0d }
-  .nick-2 { color: #e43611 }
-  .nick-3 { color: #f98a11 }
-  .nick-4 { color: #b4a700 }
-  .nick-5 { color: #89bd3b }
-  .nick-6 { color: #4ea847 }
-  .nick-7 { color: #287e52 }
-  .nick-8 { color: #117873 }
-  .nick-9 { color: #0083a7 }
-  .nick-10 { color: #2a6596 }
-  .nick-11 { color: #385189 }
-  .nick-12 { color: #434078 }
-  .nick-13 { color: #5e4279 }
-  .nick-14 { color: #7a447a }
-  .nick-15 { color: #e92980 }
-  .nick-16 { color: #ec273e }
-
-  </style>
-  <script>
-  let firstLoad = true;
-  let isMultiline = hash => /^L[0-9]+-L[0-9]+$/.test(hash);
-  function highlightLinked() {
-    for (let msg of document.querySelectorAll('.highlight')) {
-      msg.classList.remove('highlight');
-    }
-
-    let hash = location.hash;
-    if (hash.startsWith('#')) {
-      hash = hash.substring(1);
-    }
-    if (isMultiline(hash)) {
-      let parts = hash.split('-');
-      if (parts.length !== 2) {
-        return;
-      }
-      let [first, second] = parts;
-      let tbody = document.getElementById('log-tbody');
-
-      let firstIndex = -1;
-      let secondIndex = -1;
-      let children = tbody.children;
-      for (let i = 0; i < children.length; ++i) {
-        if (children[i].id === first) {
-          firstIndex = i;
-        } else if (children[i].id === second) {
-          secondIndex = i;
-        }
-      }
-      if (firstIndex > secondIndex) {
-        [firstIndex, secondIndex] = [secondIndex, firstIndex];
-      }
-      for (let i = firstIndex; i <= secondIndex; ++i) {
-        children[i].classList.add('highlight');
-      }
-      if (firstLoad) {
-        children[firstIndex].scrollIntoView();
-      }
-    } else if (hash.length > 0) {
-      let target = document.getElementById(hash);
-      if (target) {
-        target.classList.add('highlight');
-      }
-    }
-    firstLoad = false;
-  }
-
-  addEventListener('DOMContentLoaded', highlightLinked);
-  addEventListener('hashchange', highlightLinked);
-
-  addEventListener('click', e => {
-    if (e.target.classList.contains('ts')) {
-      e.preventDefault();
-      let href = e.target.href;
-
-      if (e.shiftKey && location.hash.length > 1) {
-        let hash = location.hash.substring(1);
-        let firstHash;
-        if (isMultiline(hash)) {
-          firstHash = hash.split('-')[0];
-        } else {
-          firstHash = hash
-        }
-        let secondHash = e.target.hash.substring(1);
-        let first = document.getElementById(firstHash);
-        let second = document.getElementById(secondHash);
-        let tbody = document.getElementById('log-tbody');
-        if (first?.classList.contains('msg') && second?.classList.contains('msg')) {
-          history.pushState({}, '', '#' + firstHash + '-' + secondHash);
-        }
-      } else {
-        if (href === location.href) {
-          // when clicking on currently-highlighted TS, un-highlight
-          history.pushState({}, '', location.href.substring(0, location.href.indexOf('#')));
-        } else {
-          history.pushState({}, '', e.target.href);
-        }
-      }
-      highlightLinked();
-    }
-  });
-  </script>
+  <link rel="stylesheet" href="${cssSrc}">
+  <script src="${jsSrc}"></script>
 </head>
 <body><div class="wrapper">
 <div class="sidebar">${renderSidebar(rooms, room, day, prev, next)}</div>
---
+++ resources/logs.js
@@ -0,0 +1,83 @@
+'use strict';
+
+let firstLoad = true;
+let isMultiline = hash => /^L[0-9]+-L[0-9]+$/.test(hash);
+function highlightLinked() {
+  for (let msg of document.querySelectorAll('.highlight')) {
+    msg.classList.remove('highlight');
+  }
+
+  let hash = location.hash;
+  if (hash.startsWith('#')) {
+    hash = hash.substring(1);
+  }
+  if (isMultiline(hash)) {
+    let parts = hash.split('-');
+    if (parts.length !== 2) {
+      return;
+    }
+    let [first, second] = parts;
+    let tbody = document.getElementById('log-tbody');
+
+    let firstIndex = -1;
+    let secondIndex = -1;
+    let children = tbody.children;
+    for (let i = 0; i < children.length; ++i) {
+      if (children[i].id === first) {
+        firstIndex = i;
+      } else if (children[i].id === second) {
+        secondIndex = i;
+      }
+    }
+    if (firstIndex > secondIndex) {
+      [firstIndex, secondIndex] = [secondIndex, firstIndex];
+    }
+    for (let i = firstIndex; i <= secondIndex; ++i) {
+      children[i].classList.add('highlight');
+    }
+    if (firstLoad) {
+      children[firstIndex].scrollIntoView();
+    }
+  } else if (hash.length > 0) {
+    let target = document.getElementById(hash);
+    if (target) {
+      target.classList.add('highlight');
+    }
+  }
+  firstLoad = false;
+}
+
+addEventListener('DOMContentLoaded', highlightLinked);
+addEventListener('hashchange', highlightLinked);
+
+addEventListener('click', e => {
+  if (e.target.classList.contains('ts')) {
+    e.preventDefault();
+    let href = e.target.href;
+
+    if (e.shiftKey && location.hash.length > 1) {
+      let hash = location.hash.substring(1);
+      let firstHash;
+      if (isMultiline(hash)) {
+        firstHash = hash.split('-')[0];
+      } else {
+        firstHash = hash
+      }
+      let secondHash = e.target.hash.substring(1);
+      let first = document.getElementById(firstHash);
+      let second = document.getElementById(secondHash);
+      let tbody = document.getElementById('log-tbody');
+      if (first?.classList.contains('msg') && second?.classList.contains('msg')) {
+        history.pushState({}, '', '#' + firstHash + '-' + secondHash);
+      }
+    } else {
+      if (href === location.href) {
+        // when clicking on currently-highlighted TS, un-highlight
+        history.pushState({}, '', location.href.substring(0, location.href.indexOf('#')));
+      } else {
+        history.pushState({}, '', e.target.href);
+      }
+    }
+    highlightLinked();
+  }
+});
