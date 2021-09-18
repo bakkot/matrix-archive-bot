6c128eeff06827aebd95b28664764de5919c3ffa
Kevin Gibbons <bakkot@gmail.com>
2021-09-17 19:14:00 -0700

    sql before html, split for linux

--- scripts/split-db.sh
+++ scripts/split-db.sh
@@ -6,7 +6,7 @@ indb="$1"
 outdir="$2"
 
 # for chunked mode, we need to know the database size in bytes beforehand
-bytes="$(gstat --printf="%s" "$indb")"
+bytes="$(stat --printf="%s" "$indb")"
 # bytes="$(stat -f%z "$indb")"
 # set chunk size to 10MiB (needs to be a multiple of the `pragma page_size`!)
 # smaller chunks = smaller git diffs
@@ -14,7 +14,7 @@ serverChunkSize=$((1024 * 1024))
 suffixLength=3
 mkdir -p "$outdir"
 rm -f "$outdir/db.sqlite3"*
-gsplit "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
+split "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
 
 # set request chunk size to match page size
 requestChunkSize="$(sqlite3 "$indb" 'pragma page_size')"
