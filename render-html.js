0cd7b6ddade32d499df76752e2655dab559b9d1a
Kevin Gibbons <bakkot@gmail.com>
2021-12-19 17:05:50 -0800

    sort order is only visible on search page

--- render-html.js
+++ render-html.js
@@ -144,7 +144,7 @@ function renderDay(rooms, room, day, events, prev, next, hasSearch) {
 <body><div class="wrapper">
 <div class="sidebar">${renderSidebar(rooms, room, day, prev, next)}</div>
 <div class="rhs">
-${hasSearch ? renderSearchbar(room) : ''}
+${hasSearch ? renderSearchbar(room, false) : ''}
 <div class="log"><table id="log-table"><tbody id="log-tbody">
 ${
   events.length > 0
@@ -297,7 +297,7 @@ ${renderRoomList(rooms, room)}
 </div>
 
 <div class="rhs">
-${renderSearchbar(room)}
+${renderSearchbar(room, true)}
 <div class="log">
 <table id="log-table"><tbody id="search-output">
 </tbody></table>
@@ -309,13 +309,13 @@ ${renderSearchbar(room)}
 `;
 }
 
-function renderSearchbar(room) {
+function renderSearchbar(room, isActualPage) {
   return `<div class="rhs-header">
 <span id="error" style="color: red; display:none">error</span>
-<select id="sort-order">
+${isActualPage ? `<select id="sort-order" style="display:none">
   <option value="oldest">Oldest first</option>
   <option value="newest">Newest first</option>
-</select>
+</select>` : ''}
 <input type="text" id="query" size=25 placeholder="Search ${room}">
 <a id="search-submit" class="button icon-link" title="Search">
   <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
